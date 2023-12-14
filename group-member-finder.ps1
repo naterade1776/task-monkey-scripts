989477311ce5b43f72241bd8ddc11928605d0b85
Nate K <97563728+naterade1776@users.noreply.github.com>
2023-12-14 09:57:59 -0700

    Add files via upload

---
+++ group-member-finder.ps1
@@ -0,0 +1,8 @@
+﻿cd c:
+${grp} = Read-Host "Group Name"
+$mem = Get-ADGroupMember -identity ${grp} |
+    foreach-object {
+        get-aduser $_ -properties emailaddress
+        }
+$mem | export-csv -path .\${grp}-members.csv -notypeinformation -append
+get-content -path .\${grp}-members.csv
